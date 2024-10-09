ad69e6b705e283a96d22c4f2ab45d4fbe7ed5e1e
Josh Butcher <sjwb1999@gmail.com>
2024-10-09 15:55:05 -0700

    updated grid-body and added body-4.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 window.onload = function() {
   const form = document.querySelector("form");
+  const h1 = document.getElementById("sign");
+  const h2 = document.getElementById("setting");
+  const body = document.querySelector("body");
   form.onsubmit = function(event) {
     event.preventDefault();
 
@@ -12,7 +15,6 @@ window.onload = function() {
     if (age > 21) {
       over21.removeAttribute("class");
     } else if (age === 21) {
-      window.alert("Test");
       over21.removeAttribute("class");
     } else {
       under21.removeAttribute("class");
@@ -20,5 +22,50 @@ window.onload = function() {
   }
   form.onreset = function(event) {
     location.reload();
+  }
+  h1.onclick = function(e) {
+    let test = document.getElementById("test");
+    test.setAttribute("class", "hidden");
+    test.removeAttribute("class");
+    test.style.backgroundColor = "grey";
+    test.style.margin = "2% 10%";
+    test.style.border = "solid black 3px";
+    test.style.borderRadius = "25%";
+    test.style.fontSize = "150%";
+    h1.style.backgroundColor = "red";
+    h1.style.border = "solid black 3px";
+  }
+
+  h2.onclick = function(e) {
+    e.preventDefault;
+    let menu = document.getElementById("menu");
+    menu.setAttribute("class", "hidden");
+    menu.removeAttribute("class");
+    h2.style.backgroundColor = "red";
+    h2.style.border = "solid black 3px";
+    menu.style.backgroundColor = "grey";
+    menu.style.border = "solid black 3px";
+    menu.style.margin = "2% 10%";
+    menu.style.borderRadius = "25%";
+
+    const button1 = document.getElementById("button1");
+    button1.onclick = function() {
+      window.close();
+    }
+
+    const button2 = document.getElementById("button2");
+    button2.onclick = function() {
+      body.style.transform = "rotate(5deg)";
+    }
+
+    const button3 = document.getElementById("button3");
+    button3.onclick = function() {
+      body.style.transform = "rotate(-5deg)";
+    }
+
+    const button4 = document.getElementById("button4");
+    button4.onclick = function() {
+      location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
   }
 }
